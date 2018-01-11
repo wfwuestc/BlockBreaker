61cabf2f95638a25ffd987bb970bd3df2de97e2f
Wen <wfwuestc@163.com>
2018-01-11 11:15:16 +0800

    editor 可添加多个砖块

--- js/sense/editor/editor.js
+++ js/sense/editor/editor.js
@@ -8,27 +8,40 @@ class Editor extends Scenes {
     ],]
     this.current = 0
     this.button = document.querySelector('.editor button')
-    this.x.addEventListener('change', (e) => {
-      log(e.target.value)
-      this.levels[0][this.current][0] = e.target.value
+    this.x.addEventListener('mousedown', (e) => {
+      e.target.addEventListener('mousemove', (e) => {
+        log(e.target.value)
+        this.levels[0][this.current][0] = e.target.value
+        this.blocks = loadLevel(this.game, 1, this.levels)
+      })  
      })
-    this.y.addEventListener('change', (e) => {
-      log(e.target.value)
-      this.levels[0][this.current][1] = e.target.value
+      this.y.addEventListener('mousedown', (e) => {
+      e.target.addEventListener('mousemove', (e) => {
+        log(e.target.value)
+        this.levels[0][this.current][1] = e.target.value
+        this.blocks = loadLevel(this.game, 1, this.levels)
+      })  
+     })
+    this.button.addEventListener('click',e => {
+      e.preventDefault()
+      this.current += 1
+      let origin = [0,0]
+      this.levels[0].push(origin)
+      this.blocks = loadLevel(this.game, 1, this.levels)
     })
-
+    this.blocks = loadLevel(this.game, 1, this.levels)
     // 保存编辑结果
+
     // 读取关卡
 
     // 将关卡保存在localStorage
+
     // 进入游戏模式
   }
 
   draw() {
-    var blocks = loadLevel(this.game, 1, this.levels)
-    for (var i = 0; i < blocks.length; i++){
-      var block = blocks[i]
-      log(block)
+    for (var i = 0; i < this.blocks.length; i++){
+      var block = this.blocks[i]
       if (block.alive){
         this.game.drawImage(block)
       }
